c62baca18affbf65a621332ad0ed7775516a810e
Ian Dirk Fichtner <I.Fichtner@stud.uni-heidelberg.de>
2023-04-12 22:10:18 +0200

    Transformed positions: genes -> promoter

--- code/day1/coding_exercises_day1_start.py
+++ code/day1/coding_exercises_day1_start.py
@@ -1,5 +1,6 @@
 import os
 
+# Open files to read and write
 file = open("../../data/gencode.v19.annotation.gtf", "rt")
 
 os.system("mkdir -p ../../output/day1")
@@ -9,20 +10,27 @@ out = open("../../output/day1/gencode.v19.annotation_promoters.bed", "w")
 # Metadata
 out.write("##description: Gencode human annotated genome v19 transformed to .bed format\n##date: 2023-04-12\n")
 
+# GTF -> BED, filter and transform for promoters
 for line in file:
 
+    # Remove metadata lines
     if line.startswith("##"):
         continue
 
-
+    # Read line
     list_gtf = line.split("\t")
+
+    # Format new line
     mylist = [list_gtf[i] for i in [0,2,3,4,6]]
 
+    # Transform gene to promoter positions
+    mylist[3] = int(mylist[3]) + 100
+
     mylist[2] = int(mylist[2])
-    mylist[2] = int(mylist[2]) - 1
+    mylist[2] = int(mylist[2]) - 500 - 1 # -1 because we switch from a 0-based to a 1-based coordinate notation
 
-    mylist[3] = int(mylist[3])
 
+    # Filter for genes
     if mylist[1] != "gene":
         continue
     del mylist[1]
