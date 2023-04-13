fbe928112dde664d1079d4b20234b1e8c99e9a76
Ian Dirk Fichtner <I.Fichtner@stud.uni-heidelberg.de>
2023-04-13 14:06:55 +0200

    Exercises day1 step 4

--- code/day1/coding_exercises_day1_step3-4.sh
+++ code/day1/coding_exercises_day1_step3-4.sh
@@ -6,6 +6,7 @@ mkdir -p ../../output/day1
 # Format cpgIsland raw file
 cut -f 2,3,4 ../../data/cpgIslandExt.txt > ../../output/day1/cpgIslandExt.bed
 
+# STEP 3
 # Bedtools intersect: a='reference sequence'=promoters b='sequence to compare'=cpgIslands
 
 # Promoters intersect cpgIslands
@@ -13,3 +14,16 @@ bedtools intersect -wa -a ../../output/day1/gencode.v19.annotation_promoters.bed
 
 # Promoters diff cpgIsland
 bedtools intersect -v -a ../../output/day1/gencode.v19.annotation_promoters.bed -b ../../output/day1/cpgIslandExt.bed > ../../output/day1/cpgIsland_promoters_difference.bed
+
+# STEP 4
+mkdir -p ../../output/day1/{CgiProm,NonCgiProm}
+
+for chr in $(cut -f 1 ../../output/day1/cpgIsland_promoters_intersection.bed | uniq)
+do
+	grep -P "$chr\s"  ../../output/day1/cpgIsland_promoters_intersection.bed > ../../output/day1/CgiProm/cpgIsland_promoters_intersection_$chr.bed
+done
+
+for chr in $(cut -f 1 ../../output/day1/cpgIsland_promoters_difference.bed | uniq)
+do
+	grep -P "$chr\s"  ../../output/day1/cpgIsland_promoters_difference.bed > ../../output/day1/NonCgiProm/cpgIsland_promoters_difference_$chr.bed
+done
